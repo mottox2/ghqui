d21dbecf931180515640475666748fc2379b3c96
mottox2 <y.mottox2@gmail.com>
2018-08-25 00:03:50 +0900

    Add ability to launch editor

---
+++ src/index.d.ts
@@ -0,0 +1,4 @@
+declare module 'react-dev-utils/launchEditor.js' {
+  const launchEditor: (path: string, lineNumber: number) => void
+  export default launchEditor
+}
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -4,29 +4,45 @@
 
 import * as child_process from "child_process";
 import * as React from "react";
+import * as launchEditor from "react-dev-utils/launchEditor.js";
 import * as ReactDom from "react-dom";
 
 interface IState {
+  root: string;
   repos: string[];
 }
 
 class App extends React.Component<{}, IState> {
   public state = {
     repos: [""],
+    root: "",
   };
 
   public async componentDidMount() {
+    const root: string = (await executeCommand("ghq", ["root"])).replace("\n", "");
     const result: string = await executeCommand("ghq", ["list"]);
     const repos = result.split("\n");
-    this.setState({ repos });
+    this.setState({ root, repos });
   }
 
   public render() {
+    console.log(this.state);
     return (
       <div>
         <h1>Your Repos</h1>
         {this.state.repos.map((repo) => {
-          return <li>{repo}</li>;
+          return (
+            <li
+              onClick={() => {
+                // HACK: Avoid errors of TypeScript.
+                const launch: any = launchEditor;
+                const fileName = `${this.state.root}/${repo}`;
+                launch(fileName, 1, 1);
+              }}
+            >
+              {repo}
+            </li>
+          );
         })}
       </div>
     );
@@ -39,7 +55,6 @@ const executeCommand = (cmd: string, args: string[], opts = {}): Promise<string>
     console.log("child:" + proc.pid);
     let result: string = "";
     proc.stdout.on("data", (data) => {
-      console.log(data.toString().split("\n"), result);
       result += data.toString();
     });
     proc.on("close", (code) => {
